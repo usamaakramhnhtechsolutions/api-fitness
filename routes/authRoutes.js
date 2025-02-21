f35e04e72f37a7ae652588bef091b0f4ad3721d2
usamaakramhnhtechsolutions <https://github.com/usamaakramhnhtechsolutions/api-fitness>
2025-02-21 15:58:37 +0500

    push

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,22 +11,16 @@ require("dotenv").config();
 router.post("/register", async (req, res) => {
     try {
       const { name, email, password, image } = req.body;
-  
       // Set a default image if none is provided
       const userImage = image || "../asset/exercises.jpg"; // Default image URL
-  
       let user = await User.findOne({ email });
       if (user) {
         return res.status(400).json({ message: "User already exists" });
       }
-  
       // const hashedPassword = await bcrypt.hash(password, 10);
       user = await User.create({ name, email, password, image: userImage });
-  
       await user.save();
-  
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-  
       res.status(201).json({ user, auth: token });
     } catch (err) {
       console.error("Server Error:", err);
@@ -41,27 +35,22 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-
     // Validate input
     if (!email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-
     // Check if user exists
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
-
     // Password verification
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
-
     // Generate JWT Token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "2d" });
-
     res.json({ user: { id: user._id, name: user.name, email: user.email }, auth: token });
   } catch (error) {
     console.error("Login Error:", error);
